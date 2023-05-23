97408df772d5c086a6460756c7b17a153c45c1a1
Muhammad Urwah <urwahpatel@gmail.com>
2023-05-24 01:18:36 +0500

    Updated API for Proper Use

--- routers/router.js
+++ routers/router.js
@@ -1,10 +1,11 @@
-const { addProduct, deleteProduct, getProductsByCategory, getAllProducts } = require('../controllers/productController')
+const { addProduct, deleteProduct, getProductsByCategory, getAllProducts, getProductById } = require('../controllers/productController')
 
 const router = require('express').Router()
 
 router.post('/product', addProduct)
 router.delete('/product/:id', deleteProduct)
 router.get('/products/:category', getProductsByCategory)
-router.get('/products/', getAllProducts)
+router.get('/product/:id', getProductById)
+router.get('/products', getAllProducts)
 
 module.exports = router
